49bef27a57d0e521d349c81a968f5e0bf93568c6
kaisabbes <kaisabbes31@gmail.com>
2025-10-15 20:18:38 +0100

    Création du service de multiplication

--- src/clientPackage/Client.java
+++ src/clientPackage/Client.java
@@ -1,16 +1,30 @@
 package clientPackage;
-
 import java.io.*;
 import java.net.*;
-
+import java.util.Scanner;
 public class Client {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         try {
-            System.out.println("Client en cours de connexion...");
+            System.out.println("Je suis un client pas encore connecté…");
             Socket socket = new Socket("localhost", 1234);
-            System.out.println("Connecté au serveur !");
+            System.out.println("Je suis un client connecté !");
+            DataInputStream is = new DataInputStream(socket.getInputStream());
+            DataOutputStream os = new DataOutputStream(socket.getOutputStream());
+            int x;
+            do {
+                System.out.print("Entrez un entier (0 pour quitter) : ");
+                x = scanner.nextInt();
+                os.writeInt(x);
+                if (x != 0) {
+                    int resultat = is.readInt();
+                    System.out.println("Résultat reçu du serveur : " + resultat);
+                }
+            } while (x != 0);
+            is.close();
+            os.close();
             socket.close();
-            System.out.println("Connexion fermée. Client terminé.");
+            System.out.println("Connexion fermée.");
         } catch (IOException e) {
             System.out.println("Erreur client : " + e.getMessage());
         }
--- src/serverPackage/Server.java
+++ src/serverPackage/Server.java
@@ -1,17 +1,30 @@
 package serverPackage;
-
 import java.io.*;
 import java.net.*;
-
 public class Server {
     public static void main(String[] args) {
         try {
             ServerSocket serverSocket = new ServerSocket(1234);
-            System.out.println("Serveur en attente de connexion...");
+            System.out.println("Je suis un serveur en attente de la connexion d'un client");
             Socket clientSocket = serverSocket.accept();
+            System.out.println("Un client est connecté !");
+            DataInputStream is = new DataInputStream(clientSocket.getInputStream());
+            DataOutputStream os = new DataOutputStream(clientSocket.getOutputStream());
+            int x;
+            do {
+                x = is.readInt();
+                System.out.println("Nombre reçu du client : " + x);
+                if (x != 0) {
+                    int resultat = x * 5;
+                    System.out.println("Résultat calculé "+x+"*5) = " + resultat);
+                    os.writeInt(resultat);
+                }
+            } while (x != 0);
+            is.close();
+            os.close();
             clientSocket.close();
             serverSocket.close();
-            System.out.println("Connexion fermée. Serveur arrêté.");
+            System.out.println("Serveur fermé.");
         } catch (IOException e) {
             System.out.println("Erreur : " + e.getMessage());
         }
